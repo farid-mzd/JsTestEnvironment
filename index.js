3e9c333deed209cfdc2833099617297818ce3ca1
farid-mzd <faridbusiness@bk.ru>
2021-12-04 23:18:33 +0400

    testingObjectLiterals

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appBtn = document.getElementById('bttn');
+
+//creating objects
+//2 ways : obj cnstructor or obj literal syntax
+
+//obj literal
+let a = {
+  field1: 'stringOne',
+  field12: 15,
+};
+
+let fruit = prompt('Which fruit to buy?', 'apple');
+
+let bag = {
+  [fruit]: 5, // the name of the property is taken from the variable fruit
+};
+
+alert(bag.apple); // 5 if fruit="apple"
+console.log(a);
